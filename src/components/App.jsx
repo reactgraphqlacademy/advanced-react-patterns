10520590b25478003dc1b45a3dfe3ee3833aeb01
alexlbr <alex.lobera@gmail.com>
2017-03-06 22:08:17 +0000

    added vouchers

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,6 +9,8 @@ import MenuItem from './patterns/CompoundComponents/exercise/MenuItem'
 import Menu from './patterns/CompoundComponents/exercise/Menu'
 import Accordion from './patterns/Context/exercise/Accordion'
 import Modal from './patterns/Context/example/Modal'
+import TopVoucher from './vouchers/TopVoucher'
+import BottomVoucher from './vouchers/BottomVoucher'
 import '../assets/main.css'
 
 class App extends React.Component {
@@ -44,29 +46,14 @@ class App extends React.Component {
              isOpen={ isMenuOpen }
              toggleMenu={ this.toggleMenu }
           >
-          <p className="text-center">
-            <a target="_blank" href="https://reactjs.academy/react-redux-training-paris">
-              <img src="/public/logo.png" />
-              <br />
-              ReactJS.Academy
-            </a>
-          </p>
-          <span className="text-center">voucher</span>
-          <h3 className="text-center">
-            <a target="_blank" href="https://reactjs.academy/react-redux-training-paris">parislovesreact</a>
-          </h3>
-          <hr />
+           <TopVoucher />
            <MenuItem link="#1" toggleMenu={ this.toggleMenu }>1. Higher-Order Components</MenuItem>
            <MenuItem link="#2" toggleMenu={ this.toggleMenu }>2. Render Callback</MenuItem>
            <MenuItem link="#3" toggleMenu={ this.toggleMenu }>3. Compound Components</MenuItem>
            <Accordion header="I'm an accordion">
               <MenuItem link="#4" toggleMenu={ this.toggleMenu }>4. Context</MenuItem>
            </Accordion>
-           <hr />
-           <p className="text-center">ReactJS Academy voucher:</p>
-           <h3 className="text-center">
-             <a target="_blank" href="https://reactjs.academy/react-redux-training-paris">parislovesreact</a>
-           </h3>
+           <BottomVoucher />
           </Menu>
           <div style={ styles } className="view-container">
             <h1>Advanced React Patterns</h1>
---
+++ src/components/vouchers/TopVoucher.jsx
@@ -0,0 +1,22 @@
+import React from "react"
+
+const TopVoucher = () => (
+  <div>
+    <p className="text-center">
+      <a target="_blank" href="https://reactjs.academy/react-redux-training-paris">
+        <img src="/public/logo.png" />
+        <br />
+        ReactJS.Academy
+      </a>
+    </p>
+    <p className="text-center">
+      voucher
+    </p>
+    <h3 className="text-center">
+      <a target="_blank" href="https://reactjs.academy/react-redux-training-paris">parislovesreact</a>
+    </h3>
+    <hr />
+  </div>
+)
+
+export default TopVoucher
