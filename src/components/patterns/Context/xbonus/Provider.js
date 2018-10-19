2c99291bb9c9c21d2b34e8016578ab2d6ff49cd1
alexlbr <alex.lobera@gmail.com>
2018-10-19 22:27:01 +0100

    updates comments in  src/components/patterns/Context/xbonus/Provider.js

--- src/components/patterns/Context/xbonus/Provider.js
+++ src/components/patterns/Context/xbonus/Provider.js
@@ -2,5 +2,7 @@ import React from 'react'
 
 export const Provider = ({ store }) => {
     // TODO to implement the Redux Provider set the store in the context
+    // then update the src/components/patterns/HigherOrderComponents/xbonus/connect to read
+    // the store from the context
     return null
 }
