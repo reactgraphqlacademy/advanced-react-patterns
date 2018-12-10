02611e1fb6e392629b79eee1f3ed5df8965bfbed
alexlbr <alex.lobera@gmail.com>
2018-12-10 12:36:06 +0100

    updates import src/components/patterns/HigherOrderComponents/exercise_1/List.jsx

--- src/components/patterns/HigherOrderComponents/exercise_1/List.jsx
+++ src/components/patterns/HigherOrderComponents/exercise_1/List.jsx
@@ -1,27 +1,35 @@
-import React from 'react'
-// import withData from '../../patterns/HigherOrderComponents/exercise/withData'
+import React from "react";
+// import withData from './withData'
 
 const List = ({ data = [], error, loading }) => {
   if (error) {
-    return <p>Error: {error}</p>
+    return <p>Error: {error}</p>;
   } else if (loading) {
-    return <p>Loading...</p>
+    return <p>Loading...</p>;
   } else if (!data.length) {
-    return <p><strong>Empty list :(</strong></p>
-  } else  {
+    return (
+      <p>
+        <strong>Empty list :(</strong>
+      </p>
+    );
+  } else {
     return (
       <ul className="issue-list">
         {data.map(item => (
           <li key={item.id}>
             <a href={item.html_url} target="_blank">
-              <img alt={item.owner.login} src={item.owner.avatar_url} style={{ maxWidth: '100px' }}/>
+              <img
+                alt={item.owner.login}
+                src={item.owner.avatar_url}
+                style={{ maxWidth: "100px" }}
+              />
               {item.description}
             </a>
           </li>
         ))}
       </ul>
-    )
+    );
   }
-}
+};
 
-export default List
+export default List;
