a96d2a2557d8dc0419021dade565b8716c4d9d87
Horacio Herrera <me@hherrerag.com>
2019-01-24 14:09:30 +0100

    Accordion component updated

--- src/components/patterns/Context/exercise/components/Accordion.jsx
+++ src/components/patterns/Context/exercise/components/Accordion.jsx
@@ -1,2 +1,13 @@
 import React from 'react'
-export default () => null
+
+const Accordion = (props) => (
+  <div>
+    <p>
+      { props.header }
+      <i className="fa fa-arrow-circle-o-down pull-right"></i>
+    </p>
+    { props.children }
+  </div>
+)
+
+export default Accordion
