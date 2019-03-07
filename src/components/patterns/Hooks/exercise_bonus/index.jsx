702827ba55f7d0434f6d873eb15d3eabb24a365a
alexlbr <alex.lobera@gmail.com>
2019-03-07 08:52:56 +0000

    updates bonus hooks

--- src/components/patterns/Hooks/exercise_bonus/index.jsx
+++ src/components/patterns/Hooks/exercise_bonus/index.jsx
@@ -4,6 +4,7 @@ import WithWidth from "./WithWidth";
 const Bonus = () => (
   <React.Fragment>
     <h3>Bonus</h3>
+    <h4>Part 1</h4>
     <p>
       Refactor the <code>src/patterns/Hooks/bonus/WithWidth.jsx</code> using the{" "}
       <code>useState</code> and <code>useEffect</code> Hooks.
@@ -11,6 +12,14 @@ const Bonus = () => (
     <p>
       Tip: to remove the event listeners we need to use the clean up phase of
       the effect. To clean up the side effects you must return a function.
+    </p>
+    <h4>Part 2</h4>
+    <p>
+      Refactor the WithWidth into a custom Hook called useWidth that can be
+      reused. Documentation{" "}
+      <a href="https://reactjs.org/docs/hooks-custom.html">
+        https://reactjs.org/docs/hooks-custom.html
+      </a>
     </p>
     <WithWidth />
   </React.Fragment>
