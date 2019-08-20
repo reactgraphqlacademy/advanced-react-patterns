d394b6be987bfc00865feb7bb1f6304d797a4cd2
alexlbr <alex.lobera@gmail.com>
2019-08-20 18:17:42 +0100

    updates src/components/patterns/Hooks/exercise_custom/index.jsx

--- src/components/patterns/Hooks/exercise_custom/index.jsx
+++ src/components/patterns/Hooks/exercise_custom/index.jsx
@@ -1,45 +1,51 @@
 import React from "react";
-import WithWidth, { LARGE, MEDIUM } from "./WithWidth";
+import useWidth, { LARGE, MEDIUM } from "./useWidth";
 
-const Bonus = () => (
-  <React.Fragment>
-    <h3>Exercise custom hooks</h3>
-    <h4>Part 1</h4>
-    <p>
-      Refactor the <code>src/patterns/Hooks/bonus/WithWidth.jsx</code> using the{" "}
-      <code>useState</code> and <code>useEffect</code> Hooks.
-    </p>
-    <p>
-      Tip: to remove the event listeners we need to use the clean up phase of
-      the effect. To clean up the side effects you must return a function.
-    </p>
-    <h4>Part 2</h4>
-    <p>
-      Refactor the WithWidth into a custom Hook called useWidth that can be
-      reused. Documentation{" "}
-      <a href="https://reactjs.org/docs/hooks-custom.html">
-        https://reactjs.org/docs/hooks-custom.html
-      </a>
-    </p>
-    <WithWidth>
-      {width =>
-        width === LARGE ? (
-          <h1>I'm a large screen</h1>
-        ) : width === MEDIUM ? (
-          <h1>I'm a medium screen</h1>
-        ) : (
-          <h1>I'm a small screen</h1>
-        )
-      }
-    </WithWidth>
-    <br />
-    <h4>Part 3</h4>
-    <p>
-      Replace the withWidth HoC in{" "}
-      <code>src/components/patterns/CompoundComponents/Exercise/Menu</code> with
-      your custom hook <code>useWidth</code>.
-    </p>
-  </React.Fragment>
-);
+const Bonus = () => {
+  const width = useWidth();
+  console.log("aa", width);
+  return (
+    <React.Fragment>
+      <h3>Exercise custom hooks</h3>
+      <h4>Part 1</h4>
+      <p>
+        Refactor the{" "}
+        <code>src/patterns/Hooks/exercise_custom/WithWidth.jsx</code> using the{" "}
+        <code>useState</code> and <code>useEffect</code> Hooks.
+      </p>
+      <p>
+        Tip: to remove the event listeners we need to use the clean up phase of
+        the effect. To clean up the side effects you must return a function.
+      </p>
+      <p>
+        Documentation about custom hooks{" "}
+        <a href="https://reactjs.org/docs/hooks-custom.html">
+          https://reactjs.org/docs/hooks-custom.html
+        </a>
+      </p>
+      <p>
+        Import your useWidth and replace the hardcoded{" "}
+        <code>const width = 3</code> at the top with{" "}
+        <code>const width = useWidth()</code>
+      </p>
+
+      {width === LARGE ? (
+        <h1>I'm a large screen</h1>
+      ) : width === MEDIUM ? (
+        <h1>I'm a medium screen</h1>
+      ) : (
+        <h1>I'm a small screen</h1>
+      )}
+
+      <br />
+      <h4>Part 2</h4>
+      <p>
+        Replace the withWidth HoC in{" "}
+        <code>src/components/patterns/CompoundComponents/Exercise/Menu</code>{" "}
+        with your custom hook <code>useWidth</code>.
+      </p>
+    </React.Fragment>
+  );
+};
 
 export default Bonus;
